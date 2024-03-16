10ba3257936662765f1f90468ad111026305cbec
Hyunchan Park <hyunchan.park@jbnu.ac.kr>
2024-03-16 22:19:28 +0900

    for issue #1

--- rebuild.sh
+++ rebuild.sh
@@ -8,15 +8,16 @@ python3 manage.py compilemessages
 python3 manage.py compilejsi18n
 
 
+./reload.sh
 # 사용자에게 실행 여부 묻기
-read -p "Do you want to reload nginx? (y/n): " choice
+#read -p "Do you want to reload nginx? (y/n): " choice
 
 # 사용자 입력이 y인지 확인
-if [[ $choice == "y" ]]; then
+#if [[ $choice == "y" ]]; then
     # sudo systemctl reload nginx 실행
-    sudo systemctl reload nginx
-    echo "Nginx reloaded"
-else
-    echo "Nginx reload cancelled"
-fi
+#    sudo systemctl reload nginx
+#    echo "Nginx reloaded"
+#else
+#    echo "Nginx reload cancelled"
+#fi
 
--- reload.sh
+++ reload.sh
@@ -1,4 +1,6 @@
 sudo systemctl restart supervisor
 sudo systemctl reload nginx
+sudo systemctl restart dmoj-judge.service
+sleep 3
 
 sudo supervisorctl status
